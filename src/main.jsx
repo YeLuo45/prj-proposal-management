ea8419409e9648c608a56b2f848246f6901d7271
YeLuo45 <yeluo45@github.com>
2026-05-03 01:30:57 +0800

    fix: use HashRouter for GitHub Pages sub-path compatibility

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import ReactDOM from 'react-dom/client';
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import App from './App.jsx'
 import KanbanBoard from './pages/KanbanBoard.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/todos" element={<KanbanBoard />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 )
